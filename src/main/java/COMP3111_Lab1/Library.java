36b87c40bb87fda7a9621c6d47271c1b6ae33d4d
XU, Zuyu <porky_xu_2011@126.com>
2022-09-17 18:17:43 +0800

    Little Change

--- src/main/java/COMP3111_Lab1/Library.java
+++ src/main/java/COMP3111_Lab1/Library.java
@@ -6,8 +6,11 @@ package COMP3111_Lab1;
 public class Library {
 	
 	public static void main(String arg[]) {
-		System.out.println("hello, world!");
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
+			System.out.println("hello, world!");
 	}
+	
     public boolean someLibraryMethod() {
         return true;
     }
